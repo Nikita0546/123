e1049daa98f7a38c5c4e8cfcf0369246c379e03d
Nikita0546 <116941923+Nikita0546@users.noreply.github.com>
2023-02-12 18:09:41 +0300

    Create StrDemo.java

---
+++ ВШП/ru/StrDemo.java
@@ -0,0 +1,9 @@
+package ru;
+
+public class StrDemo {
+    public static void main(String[] args) {
+        System.out.println("Первая строка\nВторяа строка");
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+    }
+}
