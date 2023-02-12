4281ba40d2afbfe3cfc0d05e23a674375892c98c
Nikita0546 <116941923+Nikita0546@users.noreply.github.com>
2023-02-12 18:09:15 +0300

    Create CharArithDemo.java

---
+++ ВШП/ru/CharArithDemo.java
@@ -0,0 +1,14 @@
+package ru;
+
+public class CharArithDemo {
+    public static void main(String[] args) {
+        char ch;
+        ch = 'L';
+        System.out.println("ch равно" + ch);
+        ch++;
+        System.out.println("значение ch изменилось на" + ch);
+        ch = 'Д';
+        ch--;
+        System.out.println("значение ch изменилось и равно" + ch);
+    }
+}
