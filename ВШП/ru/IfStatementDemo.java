3254627be2682d5a869b5339a3c303877b7e939f
Nikita0546 <116941923+Nikita0546@users.noreply.github.com>
2023-02-12 18:09:30 +0300

    Create IfStatementDemo.java

---
+++ ВШП/ru/IfStatementDemo.java
@@ -0,0 +1,16 @@
+package ru;
+
+public class IfStatementDemo {
+    public static void main(String[] args) {
+        int a, b, c, d;
+        a = 2;
+        b = 3;
+        System.out.println("a равно 2, b равно 3");
+        if (a < b) System.out.println(" a меньше b");
+        if (a > b) System.out.println("этот текст вы никогда не увидите");
+        System.out.println(" ");
+        c = a - b;
+        System.out.println("с равно -1");
+        if (c >= 0);
+    }
+}
