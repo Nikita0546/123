dabf7fbf0f88d75c560c6170bf1111d884c47588
Nikita0546 <116941923+Nikita0546@users.noreply.github.com>
2023-02-12 18:09:21 +0300

    Create DivisionDemo.java

---
+++ ВШП/ru/DivisionDemo.java
@@ -0,0 +1,12 @@
+package ru;
+
+public class DivisionDemo {
+    public static void main(String[] args) {
+        int iresult, iremain;
+        iresult = 10 / 3;
+        iremain = 10 % 3;
+        System.out.print("частное от деления 10 /3 равно ");
+        System.out.println(iresult + ", остаток равен" + iremain);
+        dresult = 10.0 / 3.0;
+    }
+}
